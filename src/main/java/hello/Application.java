3c213572ffe536f7e4caf6fa31bc860bb60e8c21
Jam Kuong <chikkuong2@gmail.com>
2022-02-24 11:52:29 +0800

    init

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -55,9 +55,84 @@ public class Application {
 
     @PostMapping("/**")
     public String index(@RequestBody ArenaUpdate arenaUpdate) {
-        System.out.println(arenaUpdate._links);
+        System.out.println(arenaUpdate._links.self.href);
+        Integer arenaX = arenaUpdate.arena.dims.get(0);
+        Integer arenaY = arenaUpdate.arena.dims.get(1);
+
+        String[][] arena = new String[arenaX][arenaY];
+
+        PlayerState me = new PlayerState();
+
+        for (Map.Entry<String, PlayerState> entry : arenaUpdate.arena.state.entrySet()) {
+            if (entry.getKey().equals(arenaUpdate._links.self.href)) {
+                me = entry.getValue();
+            }
+
+            PlayerState ps = entry.getValue();
+            arena[ps.x][ps.y] = entry.getKey();
+        }
+
+        boolean facingX = true;
+        Integer fromX = 0;
+        Integer fromY = 0;
+        Integer toX = 0;
+        Integer toY = 0;
+
+        switch (me.direction) {
+            case "N":
+                facingX = false;
+                fromY = me.y;
+                toY = me.y + 2;
+                fromX = me.x;
+                toX = me.x;
+                if (toY > arenaY) {
+                    toY = arenaY;
+                }
+                break;
+            case "S":
+                fromY = me.y - 2;
+                toY = me.y;
+                fromX = me.x;
+                toX = me.x;
+                if (fromY < 0) {
+                    fromY = 0;
+                }
+                break;
+            case "E":
+                fromY = me.y;
+                toY = me.y;
+                fromX = me.x;
+                toX = me.x + 2;
+                if (fromX > arenaX) {
+                    fromX = arenaX;
+                }
+                facingX = true;
+                break;
+            case "W":
+                fromY = me.y;
+                toY = me.y;
+                fromX = me.x - 2;
+                toX = me.x;
+                if (fromX < 0) {
+                    fromX = 0;
+                }
+                facingX = true;
+                break;
+        }
+        boolean someoneExists = false;
+        for (int i = fromX; i <= toX; i++) {
+            for (int j = fromY; j <= toY; j++) {
+                if (arena[i][j] != null && !arena[i][j].equals(arenaUpdate._links.self.href)) {
+                    someoneExists = true;
+                }
+            }
+        }
+
         String[] commands = new String[]{"F", "R", "L", "T"};
         int i = new Random().nextInt(4);
+        if (someoneExists) {
+            return "T";
+        }
         return commands[i];
     }
 
